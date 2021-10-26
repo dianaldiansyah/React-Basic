a1e2dc6c198c620f55b6577c8c2c504114d3ba36
Dian Aldiansyah <dian.aldiansyah@thejakartapost.com>
2021-10-26 17:01:23 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Dian', age: 22, belt: 'Black', id: 1},
+      { name: 'Faida', age: 21, belt: 'White', id: 2},
+      { name: 'Dida', age: 4, belt: 'Grey', id: 3}
+    ]
+  }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    })
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  componentDidMount() {
+    console.log('Component Mounted')
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated');
+    console.log(prevProps, prevState);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React App!</h1>
+        <p>Welcome :)</p>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
+      </div>
+    );
+  }
 }
 
 export default App;
